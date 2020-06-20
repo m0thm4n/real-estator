c2560695a5c049dbd8d9c2587347886949591272
Nathan <nateim3@gmail.com>
2020-06-20 14:29:21 -0400

    Initial Commit

---
+++ RealEstator/Models/Condo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RealEstator.Models
+{
+    public class Condo
+    {
+        [Key]
+        public int CondoID { get; set; }
+        public string Address { get; set; }
+        public int Rooms { get; set; }
+        public bool HasPool { get; set; }
+    }
+}
---
+++ RealEstator/Models/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RealEstator.Models
+{
+    public class Home
+    {
+        [Key]
+        public int HomeID { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public int Rooms { get; set; }
+        [Required]
+        public bool HasPool { get; set; }
+    }
+}
