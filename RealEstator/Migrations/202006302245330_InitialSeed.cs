6d9692857c8933835468f34248f27541ec3b4c70
Nathan <nateim3@gmail.com>
2020-07-01 14:02:48 -0400

    Fixed Details for Homes view

--- RealEstator/Migrations/202006302245330_InitialSeed.cs
+++ RealEstator/Migrations/202006302245330_InitialSeed.cs
@@ -1,7 +1,8 @@
+using System;
+using System.Data.Entity.Migrations;
+
 namespace RealEstator.Migrations
 {
-    using System;
-    using System.Data.Entity.Migrations;
     
     public partial class InitialSeed : DbMigration
     {
