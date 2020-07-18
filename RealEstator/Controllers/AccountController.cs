71ed86262fb53866be3ae72d7d09e3580d4a406a
Nathan Moritz <nateim3@gmail.com>
2020-07-18 12:59:04 -0400

    Downgrade Bootstrap

--- RealEstator/Controllers/AccountController.cs
+++ RealEstator/Controllers/AccountController.cs
@@ -78,7 +78,7 @@ namespace RealEstator.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -156,7 +156,7 @@ namespace RealEstator.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
--- RealEstator/Models/AccountViewModels.cs
+++ RealEstator/Models/AccountViewModels.cs
@@ -49,8 +49,8 @@ namespace RealEstator.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "UserName")]
-        public string UserName { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -73,8 +73,8 @@ namespace RealEstator.Models
         public string Email { get; set; }
 
         [Required]
-        [Display(Name = "UserName")]
-        public string UserName { get; set; }
+        [Display(Name = "Username")]
+        public string Username { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long", MinimumLength = 6)]
